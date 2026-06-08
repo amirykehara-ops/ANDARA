e5b5b774dc50dac2fec27547c9453511f4f57089
amirykehara-ops <amir.ykehara@utec.edu.pe>
2026-06-08 18:28:44 -0500

    Update route.ts

--- andara-web/src/app/api/webhook/route.ts
+++ andara-web/src/app/api/webhook/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-
+export const dynamic = "force-dynamic"
 // ========================================================
 // CONFIGURACIÓN DE PRODUCCIÓN DIRECTA (HARDCODED)
 // ========================================================
